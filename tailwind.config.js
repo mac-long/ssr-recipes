6f356a5a30b9e41663e1afeecfe7e2e7d28b6725
Mac <maclong9@icloud.com>
2023-05-28 18:26:58 +0100

    chore: assets, base dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
-  plugins: [],
-}
+	content: [
+		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		extend: {
+			animation: {
+				fadeIn: "fadeIn .3s ease-in",
+				slideLeft: "slideLeft .5s ease-in",
+				slideRight: "slideRight .5s ease-in",
+			},
+			keyframes: {
+				fadeIn: {
+					"0%": { opacity: 0 },
+					"100%": { opacity: 1 },
+				},
+				slideLeft: {
+					"0%": { opacity: 0, transform: "translateX(-50px)" },
+					"100%": { opacity: 1, transform: "translateX(0)" },
+				},
+				slideRight: {
+					"0%": { opacity: 0, transform: "translateX(110%)" },
+					"100%": { opacity: 1, transform: "translateX(0)" },
+				},
+			},
+		},
+	},
+	plugins: [require("@tailwindcss/typography")],
+};
