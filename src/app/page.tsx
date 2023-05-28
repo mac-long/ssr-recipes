67921bc27fbbb66370d778f315730d1448272f9a
Mac <maclong9@icloud.com>
2023-05-28 22:38:53 +0100

    chore: dat fetching of multiple recipes added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
-import { getRecipeById } from "@/lib/kysely";
+import { getAllRecipes, getRecipeById } from "@/lib/kysely";
 
 export default async function Home() {
-	console.log("🚀 ~ file: page.tsx:5 ~ Home ~ recipes:", recipe);
-
+	const recipe = await getAllRecipes("de");
 	return (
 		<>
-			<h1>{`test${JSON.stringify(recipe)} `}</h1>
+			<h1>{JSON.stringify(recipe)}</h1>
 		</>
 	);
 }
--- src/lib/kysely.tsx
+++ src/lib/kysely.tsx
@@ -6,7 +6,6 @@ interface RecipientTable {
 	name: string;
 	email: string;
 	email_verified: true;
-	created_on: ColumnType<Date, string | undefined, never>;
 }
 
 interface RecipeTable {
@@ -32,7 +31,6 @@ interface RecipeTranslationTable {
 	cuisine: string;
 	ingredients: string[];
 	instructions: string[];
-	created_on: ColumnType<Date, string | undefined, never>;
 }
 
 export interface Database {
@@ -52,10 +50,35 @@ export const getRecipeById = (id: number, locale: string) =>
 			"recipe.id"
 		)
 		.select([
-			"recipe.id",
+			"id",
+			"recipe_translation.meal as meal",
 			"recipe_translation.title as title",
-			"recipe_translation.summary as summary"
+			"recipe_translation.summary as summary",
+			"recipe_translation.ingredients as ingredients",
+			"creation_time",
+			"recipe_translation.instructions as instructions",
+			"image"
 		])
 		.where("recipe.id", "=", id)
 		.where("recipe_translation.language_code", "=", locale)
 		.executeTakeFirst();
+
+export const getAllRecipes = (locale: string) =>
+	db
+		.selectFrom("recipe")
+		.innerJoin(
+			"recipe_translation",
+			"recipe_translation.recipe_id",
+			"recipe.id"
+		)
+		.select([
+			"recipe.id",
+			"recipe_translation.meal as meal",
+			"recipe_translation.cuisine as cuisine",
+			"recipe_translation.title as title",
+			"recipe_translation.summary as summary",
+			"recipe_translation.ingredients as ingredients",
+			"recipe.created_on"
+		])
+		.where("recipe_translation.language_code", "=", locale)
+		.execute();
