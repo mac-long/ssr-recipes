4d25731dbfe44829662fb8f3073851fb790d1919
mac <mac@3sidedcube.com>
2023-05-30 07:13:28 +0100

    deps: installed storybook + addons + icons chore: configured storybook

---
+++ .storybook/main.ts
@@ -0,0 +1,31 @@
+import type { StorybookConfig } from "@storybook/nextjs";
+const config: StorybookConfig = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
+    "@storybook/addon-a11y",
+    "@storybook/addon-docs",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    "@storybook/addon-interactions",
+    "@storybook/addon-links",
+    "@storybook/addon-links",
+    "@storybook/addon-links",
+    "@storybook/addon-storysource",
+    "@storybook/test-runner",
+    "storybook-mobile",
+    {
+      name: '@storybook/addon-styling',
+      options: {
+        postCss: true
+      }
+    }
+  ],
+  framework: {
+    name: "@storybook/nextjs",
+    options: {},
+  },
+  docs: {
+    autodocs: "tag",
+  },
+};
+export default config;
