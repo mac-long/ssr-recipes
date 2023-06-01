896ae94250f9f0c4071086205353b009ef2f5bf9
mac <mac@3sidedcube.com>
2023-06-01 15:52:37 +0100

    feat: CerationTime and Stories Added

--- src/components/Atoms/Recipe/CreationTime/CreationTime.stories.tsx
+++ src/components/Atoms/Recipe/CreationTime/CreationTime.stories.tsx
@@ -4,22 +4,22 @@ import CreationTime from ".";
 export default {
 	title: "Components/Atoms/Creation Time",
 	component: CreationTime,
-  args: {
-    time: "90 minutes"
-  },
+	args: {
+		time: "90 minutes",
+	},
 	tags: ["autodocs"],
-  decorators: [
-    Story => (
-      <main className="prose">
-        <Story />
-      </main>
-    )
-  ]
+	decorators: [
+		(Story) => (
+			<main className="prose">
+				<Story />
+			</main>
+		),
+	],
 } as Meta<typeof CreationTime>;
 
 export const Default = {};
 export const OtherTime = {
-  args: {
-    time: "1.5 hours"
-  }
+	args: {
+		time: "1.5 hours",
+	},
 };
--- src/components/Atoms/Recipe/CreationTime/index.tsx
+++ src/components/Atoms/Recipe/CreationTime/index.tsx
@@ -1,15 +1,13 @@
 import { ClockIcon } from "@heroicons/react/24/outline";
 
-export default function CreationTime({time}: {time: string}) {
-  return (
-    <>
-    	<h2>Creation Time</h2>
+export default function CreationTime({ time }: { time: string }) {
+	return (
+		<>
+			<h2>Creation Time</h2>
 			<span className="flex items-center font-bold leading-8 text-slate-700">
-				<ClockIcon className="w-6 h-6 mr-1" />
-				<span>
-          {time}
-				</span>
+				<ClockIcon className="mr-1 w-6 h-6" />
+				<span>{time}</span>
 			</span>
-    </>
-  )
+		</>
+	);
 }
