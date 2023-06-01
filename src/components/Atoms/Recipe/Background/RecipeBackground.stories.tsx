29705374e9812fe74db992950f5c1f3b3e98286f
mac <mac@3sidedcube.com>
2023-06-01 15:32:48 +0100

    feat: added recipe background and story

---
+++ src/components/Atoms/Recipe/Background/RecipeBackground.stories.tsx
@@ -0,0 +1,10 @@
+import type { Meta } from "@storybook/react";
+import RecipeBackground from ".";
+
+export default {
+	title: "Components/Atoms/Background",
+	component: RecipeBackground,
+	tags: ["autodocs"],
+} as Meta<typeof RecipeBackground>;
+
+export const Default = {};
