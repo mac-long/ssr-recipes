44af29beb01e191d5fc1bf5e0879e5282188a9b4
mac <mac@3sidedcube.com>
2023-06-02 14:26:08 +0100

    feat: Ingredients and Instructions

--- src/components/Atoms/Recipe/Ingredients/Ingredients.stories.tsx
+++ src/components/Atoms/Recipe/Ingredients/Ingredients.stories.tsx
@@ -6,21 +6,21 @@ export default {
 	title: "Components/Atoms/Recipe/Ingredients",
 	component: Ingredients,
 	args: {
-    ingredients: [
-      "1 cup of flour",
-      "1 cup of sugar",
-      "1 cup of butter",
-      "1 cup of love",
-    ],
+		ingredients: [
+			"1 cup of flour",
+			"1 cup of sugar",
+			"1 cup of butter",
+			"1 cup of love",
+		],
 	},
-  decorators: [
-    (Story) => (
-      <main className="propse">
-        <Story />
-      </main>
-    )
-  ],
-	tags: ["autodocs"]
+	decorators: [
+		(Story) => (
+			<main className="propse">
+				<Story />
+			</main>
+		),
+	],
+	tags: ["autodocs"],
 } as Meta<typeof CreationTime>;
 
 export const Default = {};
--- src/components/Atoms/Recipe/Instructions/Instructions.stories.tsx
+++ src/components/Atoms/Recipe/Instructions/Instructions.stories.tsx
@@ -6,22 +6,22 @@ export default {
 	title: "Components/Atoms/Recipe/Instructions",
 	component: Instructions,
 	args: {
-    instructions: [
-      "Boil water",
-      "Add pasta",
-      "Cook for 8 minutes",
-      "Drain water",
-      "Consume 👾"
-    ],
+		instructions: [
+			"Boil water",
+			"Add pasta",
+			"Cook for 8 minutes",
+			"Drain water",
+			"Consume 👾",
+		],
 	},
-  decorators: [
-    (Story) => (
-      <main className="propse">
-        <Story />
-      </main>
-    )
-  ],
-	tags: ["autodocs"]
+	decorators: [
+		(Story) => (
+			<main className="propse">
+				<Story />
+			</main>
+		),
+	],
+	tags: ["autodocs"],
 } as Meta<typeof CreationTime>;
 
 export const Default = {};
