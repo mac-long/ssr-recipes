30ba9b8cc9ea1d0fdd3291c62a35ac829ecee500
mac <mac@3sidedcube.com>
2023-06-06 16:20:41 +0100

    feat: basic Content structure

---
+++ src/components/Molecules/Recipes/Content/Content.stories.tsx
@@ -0,0 +1,11 @@
+import type { Meta } from "@storybook/react";
+import Content from ".";
+
+export default {
+	title: "Components/Molecules/Recipes/Content",
+	component: Content,
+	args: {},
+	tags: ["autodocs"],
+} as Meta<typeof Content>;
+
+export const Default = {};
---
+++ src/components/Molecules/Recipes/Content/index.tsx
@@ -0,0 +1,101 @@
+import Background from "@/components/Atoms/Recipes/Content/Background";
+import CreationTime from "@/components/Atoms/Recipes/Content/CreationTime";
+import Share from "@/components/Atoms/Share";
+import {
+	DevicePhoneMobileIcon,
+	EnvelopeIcon,
+} from "@heroicons/react/24/outline";
+import Image from "next/image";
+import Link from "next/link";
+
+export interface RecipeProps {
+	meal: string;
+	title: string;
+	image: string;
+	summary: string;
+	ingredients: string[];
+	creation_time: string;
+	instructions: string[];
+}
+
+export default function Content({
+	meal,
+	title,
+	image,
+	summary,
+	ingredients,
+	creation_time,
+	instructions,
+}: RecipeProps) {
+	return (
+		<div className="overflow-hidden relative py-32 px-6 bg-white lg:overflow-visible lg:px-0 isolate">
+			<Background />
+			<div className="grid grid-cols-1 gap-x-8 gap-y-16 mx-auto max-w-2xl lg:grid-cols-2 lg:gap-y-10 lg:items-start lg:mx-0 lg:max-w-none animate-slideLeft">
+				<div className="lg:grid lg:grid-cols-2 lg:col-span-2 lg:col-start-1 lg:row-start-1 lg:gap-x-8 lg:px-8 lg:mx-auto lg:w-full lg:max-w-7xl">
+					<div className="lg:pr-4">
+						<div className="lg:max-w-lg">
+							<div className="mt-2 space-x-3 text-3xl font-bold tracking-tight sm:text-4xl">
+								<h1>{title}</h1>
+								<Share
+									options={[
+										{
+											id: 1,
+											icon: <EnvelopeIcon />,
+											message: "Check out this awesome recipe I found on SSR",
+											href: "mailto:testing123",
+										},
+										{
+											id: 2,
+											icon: <DevicePhoneMobileIcon />,
+											message: "Check out this awesome recipe I found on SSR",
+											href: "sms:&body=wonderfulawesomeness.",
+										},
+									]}
+								/>
+							</div>
+							<p className="mt-6 text-xl leading-8">{summary}</p>
+							<h2>Ingredients</h2>
+							<ul>
+								{ingredients.map((ingredient) => (
+									<li key={ingredient}>{ingredient}</li>
+								))}
+							</ul>
+							<h2>Creation Time</h2>
+							<CreationTime time={creation_time} />
+						</div>
+					</div>
+					<h2>Instructions</h2>
+					<ol>
+						{instructions.map((instruction) => (
+							<li key={instruction}>{instruction}</li>
+						))}
+					</ol>
+				</div>
+				<div className="p-12 -mt-12 -ml-12 lg:sticky lg:top-4 lg:col-start-2 lg:row-span-2 lg:row-start-1 animate-slideRight">
+					<Image
+						className="max-w-none bg-gray-900 rounded-xl ring-1 shadow-2xl sm:max-w-2xl w-[64rem] ring-gray-400/10"
+						style={{ width: "auto", height: "auto" }}
+						src={image.url}
+						alt={`${title} a food for ${meal}.`}
+						width={1024}
+						height={672}
+						priority
+						placeholder="blur"
+						blurDataURL="https://placehold.co/1024x662"
+					/>
+					{/* <Link
+						className="flex items-center w-64 text-xs font-extralight"
+						href={photographer_url || "https://openai.com/dalle"}
+						target="_blank"
+					>
+						<CameraIcon className="mr-1 mb-1 w-6 h-6" />
+						{photographer_name || "DALL-E 2"}
+					</Link> */}
+				</div>
+				<Link href="/recipes" className="mx-auto button primary">
+					Back to recipes
+				</Link>
+			</div>
+		</div>
+	);
+}
