6a42a14681491fdedc93fd04091e91994e8ee274
mac <mac@3sidedcube.com>
2023-06-06 16:55:39 +0100

    chore: rm React import

--- src/components/Atoms/Feature/index.tsx
+++ src/components/Atoms/Feature/index.tsx
@@ -1,5 +1,3 @@
-import { React } from "react";
-
 export interface Props {
 	icon: React.ReactNode;
 	name: string;
--- src/components/Molecules/Recipes/List/index.tsx
+++ src/components/Molecules/Recipes/List/index.tsx
@@ -1,5 +1,3 @@
-import { React } from "react";
-
 export default function List({
 	title,
 	description,
