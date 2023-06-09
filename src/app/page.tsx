9037e46867b1d2f48f79a8dc0890933b1c2e82b6
mac <mac@3sidedcube.com>
2023-06-09 14:24:36 +0100

    chore: wahoo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,5 @@
-import Filters from "@/components/Recipes/List/Filters";
+import Header from "@/components/Layout/Header";
 
 export default async function Home() {
-<<<<<<< Updated upstream
-	return (
-		<div className="p-4">
-			<Filters
-				filters={{
-					meals: ["Breakfast", "Brunch", "Lunch", "Dinner"],
-					cuisines: ["American", "Chinese", "French", "Indian"],
-				}}
-			/>
-		</div>
-	);
-=======
 	return <Header />;
->>>>>>> Stashed changes
 }
