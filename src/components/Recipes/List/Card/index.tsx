58f0d991620f5ad04f0dc07a87e6ebf2e6b23804
mac <mac@3sidedcube.com>
2023-06-20 15:09:17 +0100

    style: RecipeCard underline

--- src/components/Recipes/List/Card/index.tsx
+++ src/components/Recipes/List/Card/index.tsx
@@ -17,7 +17,7 @@ export default function RecipeCard({
 	const dateString = new Date(created_on).toLocaleDateString();
 
 	return (
-		<Link href={`/recipes/${id}`} passHref>
+		<Link href={`/recipes/${id}`} passHref className="no-underline">
 			<div className="flex flex-row-reverse justify-between p-10 rounded-md shadow-md cursor-pointer hover:bg-gray-200 group">
 				<div className="flex flex-col justify-between items-end text-xs">
 					<time dateTime={dateString} className="text-slate-500">
