8622b453479ccee64aa2305d204d7d2bb84eade4
mac <mac@3sidedcube.com>
2023-06-20 15:08:33 +0100

    style: size and max-w of h1

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -14,9 +14,9 @@ export default function Hero({ title, description, buttons }: Props) {
 	return (
 		<div className="relative px-6 pt-28 pb-40 lg:px-8 isolate animate-fadeIn">
 			<HeroGradient />
-			<div className="py-32 mx-auto max-w-2xl sm:py-48 lg:py-56">
+			<div className="py-32 mx-auto sm:py-48 lg:py-56 sm:max-w-lg">
 				<div className="text-center">
-					<h1 className="text-4xl font-bold tracking-tight text-transparent bg-clip-text bg-gradient-to-tr from-teal-500 to-blue-500">
+					<h1 className="text-7xl font-bold tracking-tight text-transparent bg-clip-text bg-gradient-to-tr from-teal-500 to-blue-500 leading-[5rem]">
 						{title}
 					</h1>
 					<p className="mt-6 text-lg leading-8 text-slate-600 dark:text-slate-400">
