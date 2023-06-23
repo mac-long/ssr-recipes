55609becd07014d383443c5a10b088aa444585cc
mac <mac@3sidedcube.com>
2023-06-23 11:56:24 +0100

    fix: lightened text slightly aria

--- src/components/Feature/index.tsx
+++ src/components/Feature/index.tsx
@@ -13,7 +13,7 @@ export default function Feature({ icon, name, description }: Props) {
 				</div>
 				<h3 className="text-white">{name}</h3>
 			</dt>
-			<dd className="mt-2 text-base leading-7 text-slate-500">{description}</dd>
+			<dd className="mt-2 text-base leading-7 text-slate-400">{description}</dd>
 		</div>
 	);
 }
